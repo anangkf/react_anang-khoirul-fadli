3b52053f983ba6f386bd950bb49d782e5db27c97
anangkf_ <gonanggoneng@gmail.com>
2022-08-23 12:17:06 +0700

    ✨ Add greet user

--- 03_Version Control and Branch Management/praktikum/sayHi.js
+++ 03_Version Control and Branch Management/praktikum/sayHi.js
@@ -4,3 +4,9 @@ const lastName = "robertson";
 const getfullName = (firstName, lastName) =>{
     return `${firstName} ${lastName}`;
 }
+
+const greeting = (fullName) =>{
+    return (`Hi, ${fullName}!`);
+}
+
+console.log(greeting(getfullName(firstName, lastName)));
