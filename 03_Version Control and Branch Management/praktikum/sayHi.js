ae8174b23691f48205e37a101f1801ae82020c3b
anangkf_ <gonanggoneng@gmail.com>
2022-08-23 13:27:43 +0700

    ✨ Get capitalize name

--- 03_Version Control and Branch Management/praktikum/sayHi.js
+++ 03_Version Control and Branch Management/praktikum/sayHi.js
@@ -5,6 +5,13 @@ const getfullName = (firstName, lastName) =>{
     return `${firstName} ${lastName}`;
 }
 
+const capitalizeName = (firstName, lastName) =>{
+    firstName = firstName[0].toUpperCase() + firstName.substr(1);
+    lastName = lastName[0].toUpperCase() + lastName.substr(1);
+
+    return `${firstName} ${lastName}`;
+}
+
 const greeting = (fullName) =>{
     return (`Hi, ${fullName}!`);
 }
