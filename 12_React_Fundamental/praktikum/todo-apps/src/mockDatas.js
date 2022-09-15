f6ef6db2a15da42d693cbc1c1c60c018c9c7bd77
anangkf_ <gonanggoneng@gmail.com>
2022-09-15 20:07:27 +0700

    added dummy todos data

---
+++ 12_React_Fundamental/praktikum/todo-apps/src/mockDatas.js
@@ -0,0 +1,44 @@
+export const mockTodos = [
+  {
+    "id":18,
+    "todo":"Bake a pie with some friends",
+    "completed":false,
+    "userId":1
+  },
+  {
+    "id":19,
+    "todo":"Create a compost pile",
+    "completed":true,
+    "userId":5
+  },
+  {
+    "id":20,
+    "todo":"Take a hike at a local park",
+    "completed":true,
+    "userId":43
+  },
+  {
+    "id":21,
+    "todo":"Learn Express.js",
+    "completed":false,
+    "userId":49
+  },
+  {
+    "id":22,
+    "todo":"Learn calligraphy",
+    "completed":false,
+    "userId":50
+  },
+  {
+    "id":23,
+    "todo":"Research a topic interested in",
+    "completed":false,
+    "userId":4
+  },
+  {
+    "id":24,
+    "todo":"Plan a trip to another country",
+    "completed":true,
+    "userId":37
+  }
+]
